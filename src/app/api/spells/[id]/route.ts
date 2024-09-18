f404345343b0c00f3fd8129ef4930ea5e9779780
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-17 22:25:35 -0400

    implement PUT spell request

--- src/app/api/spells/[id]/route.ts
+++ src/app/api/spells/[id]/route.ts
@@ -12,3 +12,28 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ success: false, message: error });
   }
 }
+
+export async function PUT(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    if (data.id) {
+      return NextResponse.json({
+        success: false,
+        error: 'You cannot update the id',
+      });
+    }
+
+    const id = parseInt(req.url.split('/spells/')[1]);
+    const spell = await prisma.spell.update({
+      where: { id },
+      data: {
+        name: data.name,
+        description: data.description,
+      },
+    });
+    return NextResponse.json({ success: true, data: spell });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error });
+  }
+}
