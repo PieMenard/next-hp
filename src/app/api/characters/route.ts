f2b5e84dde9dbfe282fa1033e76b385f2288a752
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-18 19:32:36 -0400

    implement GET all cahracters with pagination

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -1,6 +1,30 @@
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const offset = parseInt(searchParams.get('offset') || '0');
+    const limit = parseInt(searchParams.get('limit') || '10');
+
+    const data = await prisma.character.findMany({
+      select: { name: true, id: true },
+      skip: offset,
+      take: limit,
+    });
+
+    const results = {
+      offset: offset,
+      limit: limit,
+      results: data,
+    };
+
+    return NextResponse.json({ success: true, data: results });
+  } catch (error) {
+    return NextResponse.json({ success: true, error: error });
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
